ffb9169ef382899188a8d81ab7f9f91167aae160
Jazmin Matos <jmatos1993@gmail.com>
2020-08-20 18:43:34 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -4,3 +4,11 @@ def using_until
     
 end
 
+levitation_force = 6
+
+def using_while(levitation_force)
+  while levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force+=1
+  end
+end
